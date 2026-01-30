75c1e20a61d4d5a5621e2cb2e8c53c9cc52777be
Aditey Nandan <aditeyn@iisc.ac.in>
2026-01-30 12:22:25 +0530

    fjf

--- public/static/menu.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("scroll", () => {
-    const docHeight = document.body.scrollHeight - window.innerHeight;
-    const scrolled = (window.scrollY / docHeight) * 100;
-    document.getElementById("progress-bar").style.width = scrolled + "%";
-  });
